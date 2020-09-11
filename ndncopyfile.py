b55262f08ccc47c6a6a2b54934292736e99a394f
uni2u <uni2u@nate.com>
2020-09-11 16:20:02 +0900

    rsync edit

--- ndncopyfile.py
+++ ndncopyfile.py
@@ -15,7 +15,8 @@ PATH = '/var/lib/ndn/repo/'
 
 def sync(remote):
     subprocess.call(
-        shlex.split('rsync -a --no-g --no-o {remote}:{PATH} {PATH}'),
+#        shlex.split('rsync -a --no-g --no-o {remote}:{PATH} {PATH}'),
+        shlex.split('rsync -a --no-g --no-o '+remote+':'+PATH+' '+ PATH),
         stdout=subprocess.DEVNULL)
 
 
