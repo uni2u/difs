808acd606c2fd1c91d4b1bd9a5c407e0d9da890f
labry <justin.labry@gmail.com>
2021-11-26 09:22:32 +0900

    Update auto date naming

--- difs-hc-put-benchmark-ecdsa-singlemode.py
+++ difs-hc-put-benchmark-ecdsa-singlemode.py
@@ -138,7 +138,10 @@ def run_test(size):
 sizes = [2 ** i for i in range(27, 31)]
 #sizes = [2 ** 27]
 
-with open('2021-11-25-hashchain-singlemode-ecdsa-results.tsv', 'w') as tsvfile:
+import time
+timestr = time.strftime("%Y%m%d")
+print(timestr)
+with open(f'{timestr}-hashchain-singlemode-ecdsa-results.tsv', 'w') as tsvfile:
     writer = csv.writer(tsvfile, delimiter='\t')
     for size in sizes:
         for row in run_test(size):
